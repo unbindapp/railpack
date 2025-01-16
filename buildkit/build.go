a8acc61bec8e9a534c55123ae9b1c9d304a0051d
Jake Runzer <jakerunzer@gmail.com>
2025-01-15 20:37:28 -0500

    holy mother of god I figured out the configuration needed

--- buildkit/build.go
+++ buildkit/build.go
@@ -1,15 +1,18 @@
 package buildkit
 
 import (
-	"encoding/json"
 	"fmt"
+	"io"
 	"os"
+	"strings"
 
 	"github.com/charmbracelet/log"
 	"github.com/moby/buildkit/client"
 	_ "github.com/moby/buildkit/client/connhelper/dockercontainer"
 	_ "github.com/moby/buildkit/client/connhelper/nerdctlcontainer"
 	"github.com/moby/buildkit/client/llb"
+	"github.com/moby/buildkit/session"
+	"github.com/moby/buildkit/session/filesync"
 	"github.com/moby/buildkit/util/appcontext"
 	_ "github.com/moby/buildkit/util/grpcutil/encoding/proto"
 	"github.com/railwayapp/railpack-go/core/plan"
@@ -22,6 +25,8 @@ type BuildWithBuildkitClientOptions struct {
 
 func BuildWithBuildkitClient(appDir string, plan *plan.BuildPlan, opts BuildWithBuildkitClientOptions) error {
 	ctx := appcontext.Context()
+
+	imageName := getImageName(appDir)
 
 	// Connect to buildkit daemon
 	// If running in Docker, you'll need the address of your buildkit container
@@ -47,41 +52,57 @@ func BuildWithBuildkitClient(appDir string, plan *plan.BuildPlan, opts BuildWith
 
 	log.Debugf("Buildkit version: %s", info.BuildkitVersion.Version)
 
-	llbState, image, err := ConvertPlanToLLB(plan)
-	if err != nil {
-		return fmt.Errorf("error converting plan to LLB: %w", err)
-	}
+	// llbState, image, err := ConvertPlanToLLB(plan)
+	// if err != nil {
+	// 	return fmt.Errorf("error converting plan to LLB: %w", err)
+	// }
 
-	imageBytes, err := json.Marshal(image)
-	if err != nil {
-		return fmt.Errorf("error marshalling image: %w", err)
-	}
+	// imageBytes, err := json.Marshal(image)
+	// if err != nil {
+	// 	return fmt.Errorf("error marshalling image: %w", err)
+	// }
 
-	fmt.Println(string(imageBytes))
+	// fmt.Println(string(imageBytes))
+
+	llbState := llb.Image("ubuntu:noble")
 
 	def, err := llbState.Marshal(ctx, llb.LinuxAmd64)
 	if err != nil {
 		return fmt.Errorf("error marshaling LLB state: %w", err)
 	}
 
-	log.Debugf("Local mount: %s", appDir)
-
-	appFS, err := fsutil.NewFS(appDir)
+	outputFS, err := fsutil.NewFS(appDir)
 	if err != nil {
 		return fmt.Errorf("error creating FS: %w", err)
 	}
 
 	res, err := c.Solve(ctx, def, client.SolveOpt{
-		LocalMounts: map[string]fsutil.FS{
-			"context": appFS,
-		},
 		Exports: []client.ExportEntry{
+			// {
+			// 	Type:      "local",
+			// 	OutputDir: "output",
+			// },
+			// {
+			// 	Type: "docker",
+			// 	Attrs: map[string]string{
+			// 		"name": opts.ImageName,
+			// 	},
+			// },
 			{
-				Type: "docker",
+				Type: client.ExporterDocker,
 				Attrs: map[string]string{
-					"name": opts.ImageName,
+					"name": imageName,
+					// "containerimage.config": string(imgJSON),
+				},
+				Output: func(_ map[string]string) (io.WriteCloser, error) {
+					return os.Stdout, nil
 				},
 			},
+		},
+		Session: []session.Attachable{
+			filesync.NewFSSyncProvider(filesync.StaticDirSource{
+				"output": outputFS,
+			}),
 		},
 	}, nil)
 	if err != nil {
@@ -93,4 +114,13 @@ func BuildWithBuildkitClient(appDir string, plan *plan.BuildPlan, opts BuildWith
 	// res.AddMeta(exptypes.ExporterImageConfigKey, imageBytes)
 
 	return nil
+}
+
+func getImageName(appDir string) string {
+	parts := strings.Split(appDir, string(os.PathSeparator))
+	name := parts[len(parts)-1]
+	if name == "" {
+		name = "railpack-app" // Fallback if path ends in separator
+	}
+	return name
 }
--- go.mod
+++ go.mod
@@ -18,6 +18,7 @@ require (
 	github.com/Microsoft/go-winio v0.6.2 // indirect
 	github.com/aymanbagabas/go-osc52/v2 v2.0.1 // indirect
 	github.com/charmbracelet/lipgloss v0.10.0 // indirect
+	github.com/containerd/console v1.0.4 // indirect
 	github.com/containerd/containerd v1.7.24 // indirect
 	github.com/containerd/containerd/api v1.7.19 // indirect
 	github.com/containerd/continuity v0.4.5 // indirect
@@ -29,6 +30,7 @@ require (
 	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/distribution/reference v0.6.0 // indirect
 	github.com/docker/cli v27.4.0-rc.2+incompatible // indirect
+	github.com/docker/docker-credential-helpers v0.8.2 // indirect
 	github.com/felixge/httpsnoop v1.0.4 // indirect
 	github.com/go-logfmt/logfmt v0.6.0 // indirect
 	github.com/go-logr/logr v1.4.2 // indirect
@@ -41,6 +43,7 @@ require (
 	github.com/google/uuid v1.6.0 // indirect
 	github.com/grpc-ecosystem/grpc-gateway/v2 v2.20.0 // indirect
 	github.com/hashicorp/errwrap v1.1.0 // indirect
+	github.com/hashicorp/go-cleanhttp v0.5.2 // indirect
 	github.com/hashicorp/go-multierror v1.1.1 // indirect
 	github.com/in-toto/in-toto-golang v0.5.0 // indirect
 	github.com/klauspost/compress v1.17.11 // indirect
@@ -52,6 +55,7 @@ require (
 	github.com/moby/locker v1.0.1 // indirect
 	github.com/moby/patternmatcher v0.6.0 // indirect
 	github.com/moby/sys/signal v0.7.1 // indirect
+	github.com/morikuni/aec v1.0.0 // indirect
 	github.com/muesli/reflow v0.3.0 // indirect
 	github.com/muesli/termenv v0.15.2 // indirect
 	github.com/opencontainers/go-digest v1.0.0 // indirect
@@ -64,6 +68,9 @@ require (
 	github.com/shibumi/go-pathspec v1.3.0 // indirect
 	github.com/sirupsen/logrus v1.9.3 // indirect
 	github.com/tonistiigi/fsutil v0.0.0-20241121093142-31cf1f437184 // indirect
+	github.com/tonistiigi/go-csvvalue v0.0.0-20240710180619-ddb21b71c0b4 // indirect
+	github.com/tonistiigi/units v0.0.0-20180711220420-6950e57a87ea // indirect
+	github.com/tonistiigi/vt100 v0.0.0-20240514184818-90bafcd6abab // indirect
 	go.opentelemetry.io/contrib/instrumentation/google.golang.org/grpc/otelgrpc v0.46.1 // indirect
 	go.opentelemetry.io/contrib/instrumentation/net/http/httptrace/otelhttptrace v0.46.1 // indirect
 	go.opentelemetry.io/contrib/instrumentation/net/http/otelhttp v0.53.0 // indirect
@@ -79,6 +86,7 @@ require (
 	golang.org/x/sync v0.8.0 // indirect
 	golang.org/x/sys v0.26.0 // indirect
 	golang.org/x/text v0.18.0 // indirect
+	golang.org/x/time v0.6.0 // indirect
 	google.golang.org/genproto v0.0.0-20240123012728-ef4313101c80 // indirect
 	google.golang.org/genproto/googleapis/api v0.0.0-20240701130421-f6361c86f094 // indirect
 	google.golang.org/genproto/googleapis/rpc v0.0.0-20240701130421-f6361c86f094 // indirect
