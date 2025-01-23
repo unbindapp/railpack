7c1f4ba26114853bb128c6b09fa83cbf86cf1e79
Jake Runzer <jakerunzer@gmail.com>
2025-01-22 19:43:27 -0500

    add more requirements to pip example

--- examples/python-pip/main.py
+++ examples/python-pip/main.py
@@ -1,7 +1,10 @@
 import numpy as np
 import pandas as pd
+import psycopg2
 
+print("Package versions:")
 print("numpy", np.__version__)
 print("pandas", pd.__version__)
+print("psycopg2", psycopg2.__version__)
 
 print("Hello from Python")
