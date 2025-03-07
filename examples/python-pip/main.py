92970d692ac91c30a7f29ec73075f3cdbe89e355
Jake Runzer <jakerunzer@gmail.com>
2025-03-07 00:32:20 -0500

    update snapshot tests

--- examples/python-pip/main.py
+++ examples/python-pip/main.py
@@ -1,7 +1,12 @@
 import numpy as np
 import pandas as pd
+import os
 
-print("Package versions:")
+print("PATH:", os.environ['PATH'])
+path = os.environ['PATH']
+assert path.startswith("/app/.venv/bin"), f"Expected PATH to start with /app/.venv/bin but got {path}"
+
+
 print("numpy", np.__version__)
 print("pandas", pd.__version__)
 
