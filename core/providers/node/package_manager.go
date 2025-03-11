fa1e0d00773b6d5504d005a677ed67c9bf6e6e5e
Aleks Rūtiņš <keeper@farthergate.com>
2025-03-11 10:25:03 -0400

    Node: also use all files for preinstall scripts

--- core/providers/node/package_manager.go
+++ core/providers/node/package_manager.go
@@ -44,12 +44,13 @@ func (p PackageManager) RunScriptCommand(cmd string) string {
 }
 
 func (p PackageManager) installDependencies(ctx *generate.GenerateContext, packageJson *PackageJson, install *generate.CommandStepBuilder) {
+	hasPreInstall := packageJson.Scripts != nil && packageJson.Scripts["preinstall"] != ""
 	hasPostInstall := packageJson.Scripts != nil && packageJson.Scripts["postinstall"] != ""
 	hasPrepare := packageJson.Scripts != nil && packageJson.Scripts["prepare"] != ""
 
 	// If there is a postinstall script, we need the entire app to be copied
 	// This is to handle things like patch-package
-	if hasPostInstall || hasPrepare {
+	if hasPreInstall || hasPostInstall || hasPrepare {
 		install.AddCommands([]plan.Command{
 			plan.NewCopyCommand(".", "."),
 		})
